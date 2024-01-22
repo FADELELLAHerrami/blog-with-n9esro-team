6d7c8c0819030c2388190d3ce3b1c6449469aa5b
Fadel ellah ERRAMI <fadelellaherrami@gmail.com>
2024-01-22 17:32:00 +0100

    commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
+
+
+
+
+
+
 class PagesController < ApplicationController
+  before_action :authenticate_user!
   def home
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  ressources :articles do
+    ressources :reviews 
+  end
 end
