d07c91c1af0ff449fda7e467ddd48936142db583
Essoufi-oussama <87605323+Essoufi-oussama@users.noreply.github.com>
2024-01-23 14:06:15 +0100

    modified seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,43 @@
+require 'http'
 require 'faker'
+require 'json'
 
-# Création de 10 utilisateurs et d'articles associés
+url = 'https://newsapi.org/v2/everything'
+users = []
+
 10.times do
   user = User.create(
-    firstName: Faker::Name.first_name,
-    lastName: Faker::Name.last_name,
+    firstName: Faker::Name.first_name.dup,
+    lastName: Faker::Name.last_name.dup,
     email: Faker::Internet.email,
     password: Faker::Internet.password
   )
+  users << user
+end
+
+params = {
+  q: 'Apple',
+  language: 'en',
+  sortBy: 'popularity',
+  apiKey: 'fe305566d6364e0e8fb8fc8d0c4799e4'
+}
+
+response = HTTP.get(url, params: params)
+
+
+data = JSON.parse(response.body)
+
+articles = data['articles'].first(10)
+
+data['articles'].each do |article_data|
+
+  user = users.sample
 
   article = Article.create(
-    title: Faker::Lorem.sentence,
-    background: Faker::Lorem.sentence,
+    title: article_data['title'],
+    background: article_data['urlToImage'],
     user_id: user.id,
-    body: Faker::Lorem.paragraphs.join("\n\n")
+    body: article_data['content']
   )
-
-  rand(1..5).times do
-    Review.create(
-      comment: Faker::Quote.famous_last_words,
-      rating: rand(1..5),
-      article_id: article.id 
-    )
-  end
+  puts "#{article.title} created"
 end
