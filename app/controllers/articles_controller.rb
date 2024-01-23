fe612662de8a667d964cad71cd22588338f18e2d
Essoufi-oussama <87605323+Essoufi-oussama@users.noreply.github.com>
2024-01-23 15:47:51 +0100

    added paginate

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
 
   def index
-    @articles = Article.all
+    @articles = Article.page(params[:page]).per(10)
   end
 
   def show
