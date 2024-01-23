a7abe919f3c05c701545946a2469ffc405a8b8e1
Essoufi-oussama <87605323+Essoufi-oussama@users.noreply.github.com>
2024-01-23 11:33:37 +0100

    fixed problem in article controller was miising s on index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
 
   def index
-    @article = Article.all
+    @articles = Article.all
   end
 
   def show
